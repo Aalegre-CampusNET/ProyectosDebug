67488963b3f7461aadd133ede1f9272b0ad70356
Alberto Alegre - Desktop <contacto@alberto-alegre.com>
2022-02-02 16:35:50 +0100

    Add project files.

---
+++ AleatoriosNoRepetidos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AleatoriosNoRepetidos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int units = 0;
+            Console.WriteLine("How many units do you want?");
+            units = int.Parse(Console.ReadLine());
+            int ammount = 0;
+            Console.WriteLine("How many numbers do you want?");
+            ammount = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Random random = new Random();
+            for (int i = 0; i < ammount; i++)
+            {
+                AddNumber(random, units, numbers);
+            }
+
+            Console.WriteLine("Finished!");
+        }
+        public static void AddNumber(Random random, int units, List<int> numbers)
+        {
+            int number = random.Next(10 * units);
+            if (numbers.Contains(number))
+            {
+                AddNumber(random, units, numbers);
+            }
+            else
+            {
+                numbers.Add(number);
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ DosColecciones/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DosColecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Elemento> a = new List<Elemento>();
+            List<Elemento> b = new List<Elemento>();
+            for (int i = 0; i < 10; i++)
+            {
+                Elemento temp = new Elemento();
+                temp.num = i;
+                a.Add(temp);
+            }
+            foreach (var item in a)
+            {
+                b.Add(item);
+            }
+        }
+    }
+    class Elemento
+    {
+        public int num;
+    }
+}
